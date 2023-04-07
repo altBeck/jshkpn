b37e0504706812154ad21255091ac5f9407b0f35
Kanno Beck <drkannobeck@gmail.com>
2023-04-07 11:32:53 +0100

    minor-update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
+const root = resolve (__dirname, 'src')
+const outDir = resolve (__dirname, 'dist')
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir,
+    emptyOutDir: true,
+    rollupOptions: {
+      main: resolve(root, 'index.html'),
+      about: resolve(root, 'about', 'index.html'),
+    }
+  }
 })
