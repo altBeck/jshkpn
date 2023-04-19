86d4fd9ae58edd1f63aa6d56f48a0eb63c5eaa79
Kanno Beck <drkannobeck@gmail.com>
2023-04-19 11:04:57 +0100

    update-content

--- src/pages/poa/poa.jsx
+++ src/pages/poa/poa.jsx
@@ -3,7 +3,21 @@ import './poa.css';
 
 const POA = () => {
   return (
-    <div>ABCD</div>
+    <div className="liqwid--container">
+      <div className="space--liqwid">
+        <a className="home--link" href="/">HOME</a>
+      
+        <div className="space--liqwid-content">
+
+        <p>Proof of Africa is a single-pool staking operation committed to advancing decentralization within the cardano blockchain ecosystem
+        </p>
+
+        <button className="jsh-button"><a href="https://poapool.com" target="_blank">GO TO <span className="poa--link">POA</span></a></button>
+
+
+        </div>
+      </div>
+    </div>
   )
 }
 
